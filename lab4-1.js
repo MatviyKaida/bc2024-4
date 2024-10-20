821935c36b7f22901a87f7eb2002f2e51538ff37
Matviy Kaida bc2024-2 <kajdamatvej@gmail.com>
2024-10-20 17:10:30 +0300

    Added request GET

--- lab4-1.js
+++ lab4-1.js
@@ -1,9 +1,12 @@
 const {program} = require('commander')
+const { existsSync } = require('fs')
 const fs = require('fs')
 const http = require('http')
+const path = require('path')
+const superagent = require('superagent')
+
 
 program
-    .command ('server')
     .requiredOption('-h, --host <host>')
     .requiredOption('-p, --port <port>')
     .requiredOption('-c, --cache <path>')
@@ -11,11 +14,40 @@ program
         const host = options.host;
         const port = options.port;
         const cache = options.cache;
-        const server = http.createServer((req, res) => {
-            res.statusCode = 200
+        
+        const server = http.createServer(async (req, res) => {
+            const code = req.url.slice(1)
+            const filePath = path.join(cache, `${code}.jpg`);
+
+            if (req.method === 'GET') {
+                fs.promises.readFile(filePath)
+                    .then((image) => {
+                        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                        res.end(image);
+                    })
+                    .catch((error) => {
+                        superagent.get(`https://http.cat/${code}`)
+                            .then((response) => {
+                                const image = response.body;
+                                return fs.promises.mkdir(cacheDir)
+                                    .then(() => fs.promises.writeFile(filePath, image))
+                                    .then(() => {
+                                        
+                                        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                                        res.end(image);
+                                    });
+                            })
+                            .catch(() => {
+                                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                                res.end('404 Not Found');
+                            });
+                    });
+            }
+
         });
         server.listen(port, host)
-    })
+    });
+    
 program.parse();
 
 
