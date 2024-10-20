c928602c7edff0f66cbb7d226eeb47e8b71906ec
Matviy Kaida bc2024-2 <kajdamatvej@gmail.com>
2024-10-20 18:18:08 +0300

    GET requst got full functional

--- lab4-1.js
+++ lab4-1.js
@@ -18,30 +18,29 @@ program
         const server = http.createServer(async (req, res) => {
             const code = req.url.slice(1)
             const filePath = path.join(cache, `${code}.jpg`);
-
             if (req.method === 'GET') {
+                if(existsSync(`cacheDir/${code}.jpg`)){
                 fs.promises.readFile(filePath)
                     .then((image) => {
                         res.writeHead(200, { 'Content-Type': 'image/jpeg' });
                         res.end(image);
                     })
-                    .catch((error) => {
+                }
+                    else {
                         superagent.get(`https://http.cat/${code}`)
                             .then((response) => {
                                 const image = response.body;
-                                return fs.promises.mkdir(cacheDir)
-                                    .then(() => fs.promises.writeFile(filePath, image))
-                                    .then(() => {
-                                        
-                                        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-                                        res.end(image);
-                                    });
+                                fs.promises.writeFile(filePath, image)
+                                .then(()=>{
+                                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                                    res.end('404 Not Found');
+                                })
                             })
-                            .catch(() => {
-                                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                                res.end('404 Not Found');
-                            });
-                    });
+                           
+                    }
+            }
+            else if (req.method === 'PUT'){
+
             }
 
         });
